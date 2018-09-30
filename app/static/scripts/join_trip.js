651cc0709f3bf41823a015652218de5fd7ace88d
Derek <dhklangs@gmail.com>
2018-09-30 00:45:44 +0000

    Add functionality to send a notification when 'join trip' button is pressed

---
+++ app/static/scripts/join_trip.js
@@ -0,0 +1,21 @@
+// Event handler for 'JOIN TRIP' button press
+
+$(document).ready(function () {
+  $('.join').click(function () {
+    data = $('.join').attr('data-id')
+    console.log(data);
+    console.log("Pressed");
+    $.ajax({
+      type: 'POST',
+      url: '/send_notification/' + $('.join').attr('data-id'),
+      contentType: 'application/json',
+      data: {},
+      success: function (response) {
+	alert(response.response);
+      },
+      error: function (error) {
+	alert(error);
+      }
+    });
+  });
+});
